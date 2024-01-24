8af173c89f751875be2159d82aefc0aa8027c84a
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-24 09:18:43 +0500

    makeNegative

--- codewars.js
+++ codewars.js
@@ -1,8 +1,26 @@
 
 
-1-masala
+// 1-masala
 
-function evenOrOdd(number) {
-    return   number % 2 === 0 ? "Even" :"Odd";
+// function evenOrOdd(number) {
+//     return   number % 2 === 0 ? "Even" :"Odd";
       
-    }
+//     }
+
+// 2 - masala
+
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// function makeNegative(num) {
+//     return -Math.abs(num);
+//  }
+ 
