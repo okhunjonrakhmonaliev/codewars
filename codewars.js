69b9ebdd57e75cf93d225c9e48c064da22072cae
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-01-31 22:54:19 +0500

    boolean datatype

--- codewars.js
+++ codewars.js
@@ -95,6 +95,19 @@
 //   function solution(str){
 //     return str.length > 0 ? solution(str.substring(1)) + str.charAt(0) : '';
 //   }
+
+////////////////////////////////////////////////////////////////////
+
+// 5 - masala
+
+// Complete the method that takes a boolean value and
+//  return a "Yes" string for true, or a "No" string for false.
+
+//  function boolToWord( bool ){
+//     return bool === true ? "Yes" : "No";
+//   }
+
+
   
 
 
