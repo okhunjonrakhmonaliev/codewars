d6f7057409522397c7205193e489708a67abbac8
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-02-06 13:59:41 +0500

    Remove First and Last Character

--- codewars.js
+++ codewars.js
@@ -166,3 +166,24 @@
 //       return Math.abs(number)
 //      }
 //   }
+
+///////////////////////////////////////////////
+
+// 8 - masala
+
+// It's pretty straightforward. Your goal is to create a function that removes the first 
+// and last characters of a string. You're given one parameter, the original string. 
+// You don't have to worry about strings with less than two characters.
+
+// 1.way
+
+// function removeChar(str){
+//     return str.slice(1, str.length - 1);
+//   };
+  
+// 2.way
+//     function removeChar(str){
+//         return str.substring(1, str.length-1);
+//        };
+       
+
