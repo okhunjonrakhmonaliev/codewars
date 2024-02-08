37f023067717940bd7f89c69c1f87e3f1ddf7c47
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-02-08 18:47:04 +0500

    repeat string

--- codewars.js
+++ codewars.js
@@ -186,4 +186,18 @@
 //         return str.substring(1, str.length-1);
 //        };
        
+////////////////////////////////////////////////////////
 
+// 9 - masala
+
+// Write a function that accepts an integer n and a string s as parameters, 
+// and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+// function repeatStr (n, s) {
+//     return s.repeat(n);
+//   }
