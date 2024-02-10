34ddae59213c619a3e5dff684f24ac66c831fddb
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-02-10 12:06:09 +0500

    Convert a Number to a String

--- codewars.js
+++ codewars.js
@@ -314,3 +314,42 @@
 // {
 //   return eval(value1 + operation + value2);
 // }
+
+///////////////////////////////////////////////////////////
+
+// 15 - masala 
+
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid 
+// representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+// 1.way
+// const stringToNumber = function(str){
+//     return Math.floor(str)
+  
+//   }
+
+//   2.way
+//   var stringToNumber = function(str){
+//     return parseInt(str);
+//   }
+
+//   3.way
+//   var stringToNumber = function(str){
+//     return Number(str);
+//   }
+
+//   4.way
+//   var stringToNumber = function(str){
+//     return +str;
+//   }
