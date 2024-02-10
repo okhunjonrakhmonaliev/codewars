2b0d3edf09be4fc18cfa9590b01c8e2d9c0df1d7
okhunjonrakhmonaliev <rakhmonalievo@gmail.com>
2024-02-10 11:50:31 +0500

    switch case

--- codewars.js
+++ codewars.js
@@ -295,3 +295,22 @@
 // function countSheeps(sheep) {
 //     return sheep.filter(Boolean).length
 //     }
+
+///////////////////////////////////////////////////////////
+
+// 14 -masala
+
+// function basicOp(operation, value1, value2) {
+//     switch (operation) {
+//         case '+': return value1 + value2;
+//         case '-': return value1 - value2;
+//         case '*': return value1 * value2;
+//         case '/': return value1 / value2;
+//         default: return 0;
+//     }
+// }
+
+// function basicOp(operation, value1, value2)
+// {
+//   return eval(value1 + operation + value2);
+// }
